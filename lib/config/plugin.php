e1a3824175cc89a45caa2c9eec66dde5846a4012
Serge Rodovnichenko <sergerod@gmail.com>
2019-06-05 13:28:06 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Lp',
+  'img' => 'img/lp.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopLpPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopLpPlugin extends shopPlugin
+{
+
+}
